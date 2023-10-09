edcfd2b8bdfc1fee49c325641e6ca5002f9f839d
kanzihuang <kanzihuang@qq.com>
2023-10-09 23:21:26 +0800

    test: add test for SplitSqls

--- server/internal/db/api/db_test.go
+++ server/internal/db/api/db_test.go
@@ -3,6 +3,7 @@ package api
 import (
 	"github.com/stretchr/testify/require"
 	"mayfly-go/internal/db/domain/entity"
+	"strings"
 	"testing"
 )
 
@@ -52,4 +53,44 @@ func Test_escapeSql(t *testing.T) {
 			require.Equal(t, tt.want, got)
 		})
 	}
+}
+
+func TestSplitSqls(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  string
+	}{
+		{
+			name:  "select with '\\n'",
+			input: "select 'hello\nworld';",
+		},
+		{
+			name:  "create table `my-table`",
+			input: "create table `my-table` (\n\t`my-id` bigint(20)\n)",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if len(tt.want) == 0 {
+				tt.want = tt.input
+			}
+			scanner := SplitSqls(strings.NewReader(tt.input))
+			require.True(t, scanner.Scan())
+			require.Equal(t, tt.want, scanner.Text())
+		})
+	}
+}
+
+func Test_SplitSqls_WithLongString(t *testing.T) {
+	const times = 0x1000
+	sb := strings.Builder{}
+	sb.WriteString("select '")
+	for i := 0; i < times; i++ {
+		sb.WriteString("\\n0123456789\n")
+	}
+	sb.WriteString("' from tbl")
+	scanner := SplitSqls(strings.NewReader(sb.String()))
+	require.True(t, scanner.Scan())
+	require.Equal(t, sb.String(), scanner.Text())
 }
